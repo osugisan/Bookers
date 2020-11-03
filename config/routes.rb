f314dfc1b64ab9ca5fd27158b571dd2eb22825e7
EC2 Default User <ec2-user@ip-172-31-79-239.ec2.internal>
2020-11-03 12:46:01 +0000

    フォロー・フォロワー数表示　完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'home/about'
   root 'home#top'
   devise_for :users
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:show, :edit, :update, :index] do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
 
   resources :books do
     resources :book_comments, only: [:create, :destroy]
