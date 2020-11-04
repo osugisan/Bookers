2d0b54e2541f3b03a6bfd1b89d0a015fd53a79ad
EC2 Default User <ec2-user@ip-172-31-79-239.ec2.internal>
2020-11-04 07:59:54 +0000

    search機能 start

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+
+  get "search" => "seaches#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
